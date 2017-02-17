65494bcc342b75cc4e445a279c783053dd0028e8
BenjaminRamsay <BenjaminRamsay@users.noreply.github.com>
2017-02-17 08:22:31 -0500

    Upload project files

---
+++ MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PureCloudAgent
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.loginWaitTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.vmFaxButton = new System.Windows.Forms.Button();
+            this.vmFaxCountdownLabel = new System.Windows.Forms.Label();
+            this.oneSecondTimer = new System.Windows.Forms.Timer(this.components);
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.authenticateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(372, 296);
+            this.textBox1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 372);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(371, 20);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.OnWebBrowserNavigating);
+            // 
+            // loginWaitTimer
+            // 
+            this.loginWaitTimer.Interval = 5000;
+            this.loginWaitTimer.Tick += new System.EventHandler(this.loginWaitTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(514, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "This embedded web browser is used for authentication to PureCloud. Performed auto" +
+    "matically via javascript.";
+            // 
+            // vmFaxButton
+            // 
+            this.vmFaxButton.Location = new System.Drawing.Point(12, 8);
+            this.vmFaxButton.Name = "vmFaxButton";
+            this.vmFaxButton.Size = new System.Drawing.Size(139, 23);
+            this.vmFaxButton.TabIndex = 3;
+            this.vmFaxButton.Text = "Check Voicemail and Fax";
+            this.vmFaxButton.UseVisualStyleBackColor = true;
+            this.vmFaxButton.Click += new System.EventHandler(this.vmFaxButton_Click);
+            // 
+            // vmFaxCountdownLabel
+            // 
+            this.vmFaxCountdownLabel.AutoSize = true;
+            this.vmFaxCountdownLabel.Location = new System.Drawing.Point(153, 13);
+            this.vmFaxCountdownLabel.Name = "vmFaxCountdownLabel";
+            this.vmFaxCountdownLabel.Size = new System.Drawing.Size(72, 13);
+            this.vmFaxCountdownLabel.TabIndex = 4;
+            this.vmFaxCountdownLabel.Text = "<countdown>";
+            // 
+            // oneSecondTimer
+            // 
+            this.oneSecondTimer.Enabled = true;
+            this.oneSecondTimer.Interval = 1000;
+            this.oneSecondTimer.Tick += new System.EventHandler(this.oneSecondTimer_Tick);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(332, 8);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(52, 23);
+            this.logoutButton.TabIndex = 5;
+            this.logoutButton.Text = "Logout";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // authenticateButton
+            // 
+            this.authenticateButton.Location = new System.Drawing.Point(245, 8);
+            this.authenticateButton.Name = "authenticateButton";
+            this.authenticateButton.Size = new System.Drawing.Size(81, 23);
+            this.authenticateButton.TabIndex = 6;
+            this.authenticateButton.Text = "Authenticate";
+            this.authenticateButton.UseVisualStyleBackColor = true;
+            this.authenticateButton.Click += new System.EventHandler(this.authenticateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 342);
+            this.Controls.Add(this.authenticateButton);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.vmFaxCountdownLabel);
+            this.Controls.Add(this.vmFaxButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "PureCloud Agent";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer loginWaitTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button vmFaxButton;
+        private System.Windows.Forms.Label vmFaxCountdownLabel;
+        private System.Windows.Forms.Timer oneSecondTimer;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button authenticateButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using ININ.PureCloudApi.Api;
+using ININ.PureCloudApi.Model;
+
+
+namespace PureCloudAgent
+{
+    public partial class MainForm : Form
+    {
+        // *** FILL IN  YOUR INFO HERE ***
+        private const string UserName = "";                             // YOUR DUMMY USERNAME
+        private const string Password = "";                             // YOUR DUMMY USER PASSWORD
+        private const string ClientId = "";                             // YOUR OAUTH CLIENT ID
+        private const string OrgShortName = "";                         // YOUR ORG SHORT NAME (PureCloud > Admin > Organization Settings)
+        private const string DefaultRecipientEmail = "";                // DEFAULT EMAIL ADDRESS TO RECEIVE UNASSIGNED VMs/FAXES
+        private const string EmailFromAddress = "";                     // FROM ADDRESS FOR VM/FAX EMAILS
+        private const string EmailServer = "";                          // YOUR EMAIL SERVER
+        private const int EmailPort = 587;                              // EMAIL SERVER PORT
+        private const string EmailServerUsername = "";                  // EMAIL SERVER USERNAME
+        private const string EmailServerPassword = "";                  // EMAIL SERVER PASSWORD
+        // *******************************
+
+        private readonly string nl = System.Environment.NewLine;
+        private int vmFaxCountdown;
+        private string user;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Log("Starting");
+            
+            String[] arguments = Environment.GetCommandLineArgs();
+            user = (arguments.Length >= 2) ? arguments[1] : UserName;
+            this.Text += " - " + user;            
+
+            vmFaxCountdown = Properties.Settings.Default.vmFaxInterval;
+            vmFaxCountdownLabel.Text = vmFaxCountdown.ToString();
+
+            Authenticate();            
+        }
+
+        private void Authenticate()
+        {
+            Log("Authenticating");
+
+            this.WindowState = FormWindowState.Normal;
+
+            //Redirect the browser to the login window.
+            webBrowser1.Url = new Uri("https://login.mypurecloud.com/oauth/authorize?" +
+                                            "response_type=token" +
+                                            "&client_id=" + ClientId +
+                                            "&redirect_uri=http://localhost:8085/oauth2/callback");
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var url = e.Url.ToString();
+
+            if (url.Contains("https://login.mypurecloud.com/?rid") &&
+                url.Contains("/authenticate-adv/org/" + OrgShortName))
+            {
+                loginWaitTimer.Start();
+            }
+        }
+
+        private void loginWaitTimer_Tick(object sender, EventArgs e)
+        {
+            loginWaitTimer.Stop();
+
+            AutoLogin();
+        }
+
+        private void AutoLogin()
+        {
+            string javaScript = @"
+            function submitLogin()
+            {
+                document.getElementById('email').value = '" + user + @"';
+                document.getElementById('password').value = '" + Password + @"';
+                document.getElementById('password').focus();
+                document.getElementById('password').blur();
+                document.getElementsByTagName('button')[0].click();
+            }
+                ";
+
+            HtmlDocument doc = webBrowser1.Document;
+            HtmlElement head = doc.GetElementsByTagName("head")[0];
+            HtmlElement s = doc.CreateElement("script");
+            s.SetAttribute("text", javaScript);
+            head.AppendChild(s);
+            webBrowser1.Document.InvokeScript("submitLogin");
+        }
+
+        private void OnWebBrowserNavigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            //When the web browser control navigates, check if the url is the callback and contains the access_token
+            var url = e.Url.ToString();
+            if (url.Contains("access_token"))
+            {
+                var queryString = url.Substring(url.LastIndexOf("#") + 1);
+                var props = queryString.Split('&');
+                string accessToken = props[0].Split('=')[1];
+
+                ININ.PureCloudApi.Client.Configuration.Default.AccessToken = accessToken;
+                Log("Successfully authenticated");                
+            }
+        }
+
+        private void CheckVoicemailAndFax()
+        {
+            vmFaxCountdown = Properties.Settings.Default.vmFaxInterval;
+
+            try
+            {
+                ProcessVoicemail();
+
+                ProcessFax();                
+            }
+            catch (ININ.PureCloudApi.Client.ApiException ex)
+            {
+                if (ex.ErrorCode == 401)
+                {
+                    Log("Need to authenticate first. Try again in a few seconds.");
+                    Authenticate();
+                }
+                else
+                {
+                    Log("Error: " + ex.Message);
+                }                
+            }            
+        }
+
+        private void ProcessVoicemail()
+        {
+            // Check Voicemail
+            var vmApi = new VoicemailApi();
+            var convApi = new ConversationsApi();
+            var vms = vmApi.GetMessages();
+            VoicemailMediaInfo vmMedia;
+            foreach (VoicemailMessage vm in vms.Entities.Where(x => !(x.Read ?? false)))        // Unread VMs only
+            {
+                DateTime vmDate = vm.CreatedDate != null ? ((DateTime)vm.CreatedDate).ToLocalTime() : DateTime.Now;
+
+                Log("VM from " + vm.CallerAddress + " at " + vmDate);
+
+                // Check for Group attribute so we know where to send it (default to IT if nothing found)                    
+                string group = "unknown";
+                string groupEmail = DefaultRecipientEmail;
+                var call = convApi.GetCallsCallId(vm.Conversation.Id);
+
+                CallMediaParticipant participant = null;
+                try
+                {
+                    participant = call.Participants.First(x => x.Attributes != null && x.Attributes.ContainsKey("Group"));
+                }
+                catch { }
+                
+                if (participant != null)
+                {
+                    group = participant.Attributes["Group"];
+
+                    // Try to look up the email for this group in Settings. If it's not found, just move on with the default
+                    try
+                    {
+                        groupEmail = Properties.Settings.Default[group].ToString();
+                    }
+                    catch { }
+                }
+                else
+                {
+                    participant = call.Participants[0];
+                }
+
+                // Download the WAV file
+                string fileName;
+                vmMedia = vmApi.GetMessagesMessageIdMedia(vm.Id, "WAV");
+                using (var client = new WebClient())
+                {
+                    fileName = Path.GetTempPath() +
+                        "PureCloud_VM_" + group + "_" +
+                        vmDate.ToString("yyyyMMdd-HHmmss") + ".wav";
+
+                    client.DownloadFile(vmMedia.MediaFileUri, fileName);
+                }
+
+                // Email to the proper group
+                string callerName = (vm.CallerName ?? participant.Name ?? vm.CallerAddress);
+                string subject = "Voicemail from " + callerName;
+                string body = ComposeVoicemailEmail(vmDate, group, vm.CallerAddress, callerName);
+                SendEmail(groupEmail, EmailFromAddress, body, subject, fileName);
+                Log("    Sent to " + group + ": " + groupEmail);
+
+                // Mark the VM as read so it won't get sent again next time
+                vm.Read = true;
+                vmApi.PutMessagesMessageId(vm.Id, vm);
+            }
+        }
+
+        private void ProcessFax()
+        {
+            // Check Fax
+            var faxApi = new FaxApi();
+            var faxes = faxApi.GetDocuments();
+            foreach (FaxDocument fax in faxes.Entities.Where(x => !(x.Read ?? false)))     
+            {
+                DateTime faxDate = fax.DateCreated != null ? ((DateTime)fax.DateCreated).ToLocalTime() : DateTime.Now;
+                string fromNumber = fax.CallerAddress.Replace("tel:+", "");
+                string toNumber = fax.ReceiverAddress.Replace("tel:+", "");
+
+                Log("FAX from " + fax.CallerAddress + " at " + faxDate);
+
+                // Try to look up the email for this fax # in Settings. If it's not found, just move on with the default
+                string groupEmail = DefaultRecipientEmail;                
+                try
+                {
+                    groupEmail = Properties.Settings.Default["n"+toNumber].ToString();
+                }
+                catch { }
+
+                // Download the fax file
+                string fileName;
+                DownloadResponse faxFile = faxApi.GetDocumentsDocumentIdContent(fax.Id);
+                using (var client = new WebClient())
+                {
+                    // Replace any characters not allowed in a file name
+                    string fromNumberSafeString = fromNumber
+                        .Replace(@"\", "")
+                        .Replace(@"/", "")
+                        .Replace(@":", "")
+                        .Replace(@"*", "")
+                        .Replace(@"?", "")
+                        .Replace(@"""", "")
+                        .Replace(@"<", "")
+                        .Replace(@">", "")
+                        .Replace(@"|", "")
+                        .Replace(@" ", "");
+
+                    fileName = Path.GetTempPath() +
+                        "PureCloud_FAX_" + fromNumberSafeString + "_" +
+                        faxDate.ToString("yyyyMMdd-HHmmss") + ".pdf";
+
+                    client.DownloadFile(faxFile.ContentLocationUri, fileName);
+                }
+
+                // Email to the proper group
+                string subject = "Fax from " + fromNumber;
+                string body = ComposeFaxEmail(faxDate, fromNumber, toNumber);
+                SendEmail(groupEmail, EmailFromAddress, body, subject, fileName);
+                Log("    Sent to " + toNumber + ": " + groupEmail);
+
+                // Mark the fax as read so it won't get sent again next time
+                fax.Read = true;
+                faxApi.PutDocumentsDocumentId(fax.Id, fax);
+            }
+        }
+
+        private void Logout()
+        {
+            // Log Out
+            webBrowser1.Url = new Uri("https://login.mypurecloud.com/logout");
+        }
+
+        private void oneSecondTimer_Tick(object sender, EventArgs e)
+        {
+            vmFaxCountdown--;
+            vmFaxCountdownLabel.Text = vmFaxCountdown.ToString();
+
+            if (vmFaxCountdown <= 0)
+            {                
+                CheckVoicemailAndFax();
+            }
+        }
+
+        private void vmFaxButton_Click(object sender, EventArgs e)
+        {
+            CheckVoicemailAndFax();
+        }
+
+        private void Log(string message)
+        {
+            textBox1.AppendText(message + nl);
+            textBox1.Select(textBox1.TextLength - 1, 0);
+        }
+
+        private void SendEmail(string strTo, string strFrom, string strMessage, string strSubject, string attachmentPath, bool htmlFormat = true)
+        {
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            System.Net.Mail.MailAddress mto = new System.Net.Mail.MailAddress(strTo);
+            System.Net.Mail.MailAddress mfrom = new System.Net.Mail.MailAddress(strFrom);
+            mail.Subject = strSubject;
+            mail.From = mfrom;
+            mail.To.Add(mto);
+            mail.Body = strMessage;
+            mail.IsBodyHtml = htmlFormat;
+            mail.Attachments.Add(new System.Net.Mail.Attachment(attachmentPath));
+
+            System.Net.Mail.SmtpClient mailClient = new System.Net.Mail.SmtpClient(EmailServer, EmailPort);
+            mailClient.Timeout = 1000000;
+            mailClient.EnableSsl = true;
+            mailClient.UseDefaultCredentials = false;
+            mailClient.Credentials = new System.Net.NetworkCredential(EmailServerUsername, EmailServerPassword);
+            mailClient.Send(mail);
+        }
+
+        private string ComposeVoicemailEmail(DateTime vmDate, string group, string callerNumber, string callerName)
+        {
+            string body = @"
+            <span style='font-weight:bold'>Date/Time:</span> " + vmDate.ToString("MM/dd/yy HH:mm") + @"<br />
+            <span style='font-weight:bold'>Group:</span> " + group + @"<br />
+            <span style='font-weight:bold'>Caller Name:</span> " + callerName + @"<br />            
+            <span style='font-weight:bold'>Caller #:</span> " + callerNumber + @"<br />            
+            ";
+
+            return body;
+        }
+
+        private string ComposeFaxEmail(DateTime faxDate, string fromNumber, string toNumber)
+        {
+            string body = @"
+            <span style='font-weight:bold'>Date/Time:</span> " + faxDate.ToString("MM/dd/yy HH:mm") + @"<br />            
+            <span style='font-weight:bold'>From:</span> " + fromNumber + @"<br />        
+            <br />
+            <span style='font-size:0.8em'><span style='font-weight:bold'>Sent to:</span> " + toNumber + @"</span>
+            ";
+
+            return body;
+        }
+
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+            Logout();
+        }
+
+        private void authenticateButton_Click(object sender, EventArgs e)
+        {
+            Authenticate();
+        }
+    }
+}
